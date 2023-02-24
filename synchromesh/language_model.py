2c943d4bc4fdff5874400d47590d792b4faa4eb7
Minh Tue Vo Thanh <minhtuev@gmail.com>
2023-02-24 22:15:39 +0700

    Adding mocked response output

--- synchromesh/language_model.py
+++ synchromesh/language_model.py
@@ -9,6 +9,7 @@ from typing import List
 from typing import Tuple
 from typing import Dict
 import time
+import os
 
 
 class LanguageModel:
@@ -44,6 +45,7 @@ class RandomLanguageModel(LanguageModel):
     def predict_unconstrained(self, prefix, max_tokens, stop=None):
         return ''.join(random.choices(self.vocabulary(), k=max_tokens))
 
+
 class OpenAIModel(LanguageModel):
     def __init__(self, model: str, prompt_template: str, api_key: str,
                  temperature: float = 0.0, top_p: float = 1.0, best_of: int = 1) -> None:
@@ -60,14 +62,14 @@ class OpenAIModel(LanguageModel):
             with urlopen(url) as response:
                 self.token_idx = json.loads(response.read())
             self.token_idx = {s.replace('\u0120', ' '): i
-                            for s, i in self.token_idx.items()}
+                              for s, i in self.token_idx.items()}
             self.vocab = sorted(self.token_idx.keys(), key=lambda k: self.token_idx[k])
         elif model.startswith("code"):
             url = "https://huggingface.co/SaulLu/codex-like-tokenizer/raw/main/vocab.json"
             with urlopen(url) as response:
                 self.token_idx = json.loads(response.read())
             self.token_idx = {s.replace('\u0120', ' '): i
-                            for s, i in self.token_idx.items()}
+                              for s, i in self.token_idx.items()}
             self.vocab = sorted(self.token_idx.keys(), key=lambda k: self.token_idx[k])
 
     def tokenize(self, s: str) -> List[int]:
@@ -97,14 +99,14 @@ class OpenAIModel(LanguageModel):
         prompt = f"{self.prompt_template}{prefix}"
 
         # select shortest valid tokens if valid tokens are less than 1200; 4 requests
-        if len(valid_tokens) >= 299*4:
+        if len(valid_tokens) >= 299 * 4:
             token_lens = [len(self.get_token(i)) for i in valid_tokens]
             # sort valid tokens by length
             valid_tokens = [x for _, x in sorted(zip(token_lens, valid_tokens))]
-            valid_tokens = valid_tokens[:299*4-1]
+            valid_tokens = valid_tokens[:299 * 4 - 1]
 
-        for i in range(len(valid_tokens)//299+1):
-            valid_bias = {k: 100 for k in valid_tokens[i*299:(i+1)*299]}
+        for i in range(len(valid_tokens) // 299 + 1):
+            valid_bias = {k: 100 for k in valid_tokens[i * 299:(i + 1) * 299]}
             # add a negative bias for the stop token
             valid_bias[50256] = -100
             # TODO: Using codex leads to a bug
@@ -115,7 +117,6 @@ class OpenAIModel(LanguageModel):
             for k in sorted(response_dict.keys()):
                 predictions.append(self.token_idx[k])
                 probabilities.append(response_dict[k])
-        
 
         # sort predictions by probability
         predictions = [c for _, c in sorted(zip(probabilities, predictions), key=lambda x: x[0], reverse=True)]
@@ -123,12 +124,26 @@ class OpenAIModel(LanguageModel):
         predictions = predictions[:min(top_k, len(predictions))]
         predictions = [c for c in predictions]
         probabilities = probabilities[:min(top_k, len(probabilities))]
-        return  predictions, probabilities
+        return predictions, probabilities
 
-    def predict_unconstrained(self, prefix, max_tokens, stop=None):
+    def predict_unconstrained(self, prefix, max_tokens, stop=None, mocked=False, randomized=False):
         time.sleep(0.5)
+        if mocked:
+            return self.predict_mock(randomized)
+
         prompt = f"{self.prompt_template}{prefix}"
         response = openai.Completion.create(model=self.model, prompt=prompt,
                                             temperature=self.temperature, top_p=self.top_p,
                                             best_of=self.best_of, max_tokens=max_tokens, stop=stop)
         return response.choices[0].text
+
+    def predict_mock(self, randomized=False, seed=100):
+        gold_output_files = os.listdir('gold')
+        if randomized:
+            random.seed(seed)
+            output = random.choice(gold_output_files)
+        else:
+            output = gold_output_files[0]
+        output = "./gold/" + output
+        with open(output, 'r') as f:
+            return f.read()
